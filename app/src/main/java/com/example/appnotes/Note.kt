f1b750e59417518eb42414f60d114d96e2fbe294
Matias <matias_r12@hotmail.com>
2024-05-08 12:35:53 -0300

    Creamos la clase que contiene el objeto llamado Nota y la Interfaz que va a comunicar las vistas

---
+++ app/src/main/java/com/example/appnotes/Note.kt
@@ -0,0 +1,7 @@
+package com.example.appnotes
+
+
+//ESTA CLASE VA A DEFINIR EL OBJ
+data class Note (var id: Long = 0, var description: String = "", var isFinished: Boolean = false) {
+
+}
---
+++ app/src/main/java/com/example/appnotes/OnClickListener.kt
@@ -0,0 +1,5 @@
+package com.example.appnotes
+
+interface OnClickListener {
+    fun onLongClick(note: Note)
+}
